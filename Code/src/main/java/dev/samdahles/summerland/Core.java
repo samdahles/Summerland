8f644667005fc7e7b1822a616bdc14f5868b0ed3
Sam <supersonischsam@gmail.com>
2021-10-11 11:33:07 +0200

    Set up base

--- Code/src/main/java/dev/samdahles/summerland/Core.java
+++ Code/src/main/java/dev/samdahles/summerland/Core.java
@@ -1,9 +1,36 @@
 package dev.samdahles.summerland;
 
-public class Core {
+import com.github.hanyaeger.api.Size;
+import com.github.hanyaeger.api.YaegerGame;
+
+import dev.samdahles.summerland.scenes.MainMenuScene;
+import nl.han.showcase.scenes.composing.Composing;
+import nl.han.showcase.scenes.distance.TheDistanceScene;
+import nl.han.showcase.scenes.gravity.Gravity;
+import nl.han.showcase.scenes.greatballsoffire.GreatBallsOfFire;
+import nl.han.showcase.scenes.mapsandlegends.MapsAndLegends;
+import nl.han.showcase.scenes.ridethelightning.RideTheLightningScene;
+import nl.han.showcase.scenes.roadsgoeveron.RoadsGoEverOnScene;
+import nl.han.showcase.scenes.selection.SelectionScene;
+import nl.han.showcase.scenes.shapeofyou.ShapeOfYouScene;
+import nl.han.showcase.scenes.time.Time;
+
+public class Core extends YaegerGame {
 	public static void main(String[] args) {
-		
+        launch(args);
 	}
 	
+    @Override
+    public void setupGame() {
+        setGameTitle("Summerland");
+        setSize(new Size(1920, 1440));
+        setBackgroundAudio("Music/MainTheme.mp3");
+    }
+    
+    @Override
+    public void setupScenes() {
+    	var MainMenu = new MainMenuScene(this);
+    	addScene(1, MainMenu);
+    }
 
 }
--- Code/src/main/java/dev/samdahles/summerland/scenes/MainMenuScene.java
+++ Code/src/main/java/dev/samdahles/summerland/scenes/MainMenuScene.java
@@ -1,5 +1,18 @@
 package dev.samdahles.summerland.scenes;
 
-public class MainMenuScene {
+import com.github.hanyaeger.api.scenes.StaticScene;
+
+public class MainMenuScene extends StaticScene {
+
+	@Override
+	public void setupScene() {
+		setBackgroundImage("Images/MainMenuBackground.jpg");		
+	}
+
+	@Override
+	public void setupEntities() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
