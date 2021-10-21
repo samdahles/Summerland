6752ef822ee7ef604a5a127788945d327fea9dfc
Dylan <dylan249@icloud.com>
2021-10-21 21:25:53 +0200

    Update PlayableCharacter.java

--- Code/src/main/java/dev/samdahles/summerland/entities/characters/PlayableCharacter.java
+++ Code/src/main/java/dev/samdahles/summerland/entities/characters/PlayableCharacter.java
@@ -2,22 +2,46 @@ package dev.samdahles.summerland.entities.characters;
 
 import java.util.Random;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.entities.Collided;
 import com.github.hanyaeger.api.entities.Collider;
 import com.github.hanyaeger.api.entities.Direction;
+import com.github.hanyaeger.api.entities.SceneBorderTouchingWatcher;
+import com.github.hanyaeger.api.scenes.SceneBorder;
 import com.github.hanyaeger.api.userinput.KeyListener;
 
 import dev.samdahles.summerland.Core;
 import dev.samdahles.summerland.Story.Affiliation;
 import javafx.scene.input.KeyCode;
 
-public class PlayableCharacter extends TalkingCharacter implements KeyListener, Collided {
+public class PlayableCharacter extends TalkingCharacter implements KeyListener, Collided, SceneBorderTouchingWatcher {
 	
 	public PlayableCharacter(Coordinate2D initialLocation, String name, String charFolder) {
 		super(name, charFolder, initialLocation, new Size(48, 64), 4, 3, Affiliation.GOOD);
+	}
+	
+	@Override
+	public void notifyBoundaryTouching(SceneBorder border){
+	    setSpeed(0);
+	    System.out.print("hey");
+	    switch(border){
+	        case TOP:
+	            setAnchorLocationY(1);
+	            break;
+	        case BOTTOM:
+	            setAnchorLocationY(getSceneHeight() - getHeight() - 1);
+	            break;
+	        case LEFT:
+	            setAnchorLocationX(1);
+	            break;
+	        case RIGHT:
+	            setAnchorLocationX(getSceneWidth() - getWidth() - 1);
+	        default:
+	            break;
+	        }
 	}
 	
 	@Override
@@ -25,11 +49,11 @@ public class PlayableCharacter extends TalkingCharacter implements KeyListener, 
 		if(this.getLastDirection() == Direction.UP) {
 		  this.setAnchorLocationY(this.getAnchorLocation().getY() + 2.2);
 		} else if(this.getLastDirection() == Direction.LEFT) {
-		this.setAnchorLocationX(this.getAnchorLocation().getX() + 2);
+		this.setAnchorLocationX(this.getAnchorLocation().getX() + 2.2);
 		} else if(this.getLastDirection() == Direction.RIGHT) {
-			this.setAnchorLocationX(this.getAnchorLocation().getX() - 2);
+			this.setAnchorLocationX(this.getAnchorLocation().getX() - 2.2);
 		} else if(this.getLastDirection() == Direction.DOWN) {	
-			this.setAnchorLocationY(this.getAnchorLocation().getY() - 2);
+			this.setAnchorLocationY(this.getAnchorLocation().getY() - 2.2);
 		}
 	}
 
